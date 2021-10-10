86696434b7e9fce4584604caf9e8fdab0f1f308a
Nicholaus Abel Mayenga <smartvn@smart-kali.smartVN>
2021-10-10 08:33:27 -0400

    changes of today 10/10/2021

--- config/mail.php
+++ config/mail.php
@@ -36,9 +36,9 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 465),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'SoftCenter'),
+        'name' => env('MAIL_FROM_NAME', 'SoftCenter'),
     ],
 
     /*
@@ -106,5 +106,4 @@ return [
             resource_path('views/vendor/mail'),
         ],
     ],
-
 ];
